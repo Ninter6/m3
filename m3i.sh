a52b516f262ad935eea71cd4151ad6bd4cd4829e
Ninter6 <mc525740@outlook.com>
2026-04-05 17:12:23 +0800

    annoying slash removed

--- m3i.sh
+++ m3i.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-curl -fsSLO "https://raw.githubusercontent.com/Ninter6/m3/refs/heads/main/m3.ccm" && \
-cat << EOF > m3.hpp && \
+curl -fsSLO "https://raw.githubusercontent.com/Ninter6/m3/refs/heads/main/m3.ccm" &&
+cat << EOF > m3.hpp &&
 #ifndef M3_INC
 #define M3_INC
 #include "m3.ccm"
 #undef M3_EXPORT
 #endif
 EOF
-echo "m3 has been set up successfully" || \
+echo "m3 has been set up successfully" ||
 echo "failed to set up m3"
